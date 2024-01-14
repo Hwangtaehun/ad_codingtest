0b4b614bf667f2a58a49176842d5b6b7382456b0
Hwangtaehun <r283@naver.com>
2024-01-14 20:46:17 +0900

    C12문제 풀이

--- sjC12/main.cpp
+++ sjC12/main.cpp
@@ -1,9 +1,40 @@
 #include <cstdio>
+#include <cstring>
+#include <queue>
 
 int col, row, hours;
 int data[100][100];
 
-void Input(){
+//Ç®ÀÌ
+int a1[101][101], a2[101][101];
+int rowCnt, colCnt;
+
+void ArrayPrn(){
+    for(int i = 1; i <= rowCnt; i++){
+        for(int j = 1; j <= colCnt; j++){
+            printf("%d", a1[i][j]);
+        }
+        printf("\t");
+        for(int j = 1; j <= colCnt; j++){
+            printf("%d", a2[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void Input(void){
+    freopen("input.txt", "r", stdin);
+    scanf("%d %d", &rowCnt, &colCnt);
+    for(int i = 1; i <= rowCnt; i++){
+        for(int j = 1; j <= colCnt; j++){
+            scanf("%d", &a1[i][j]);
+            a2[i][j] = a1[i][j];
+        }
+    }
+}
+
+void In(){
     freopen("input.txt", "r", stdin);
     scanf("%d %d", &col, &row);
     for(int i = 0; i < col; i++){
@@ -21,6 +52,184 @@ void Print(){
         printf("\n");
     }
     printf("\n");
+}
+
+void Fill1(int x, int y){
+    if( x < 1 || x > rowCnt || y < 1 || y > colCnt || a1[x][y] != 0){
+        return;
+    }
+
+    a1[x][y] = 2;
+    Fill1(x + 1, y);
+    Fill1(x - 1, y);
+    Fill1(x, y + 1);
+    Fill1(x, y - 1);
+}
+
+int SideCount(int x, int y){
+    int cnt = 0;
+    if(a1[x + 1][y] == 2)
+        cnt++;
+    if(a1[x - 1][y] == 2)
+        cnt++;
+    if(a1[x][y + 1] == 2)
+        cnt++;
+    if(a1[x][y - 1] == 2)
+        cnt++;
+    return cnt;
+}
+
+void Solve1(){
+    int i, j, hour = 0, flag;
+
+    while(1){
+        Fill1(1, 1);
+        flag = 0;
+
+        for(i = 1; i <= rowCnt; i++){
+            for(j = 1; j <= colCnt; j++){
+                if(a1[i][j] == 1 && SideCount(i, j) >= 2){
+                    a2[i][j] = 0;
+                    flag = 1;
+                }
+            }
+        }
+
+        if(flag == 0){
+            printf("%d", hour);
+            break;
+        }
+        hour++;
+
+        ArrayPrn();
+        memcpy((void *)a1, (void *)a2, sizeof(a2));
+    }
+}
+
+void Fill2(int x, int y){
+    if( x < 1 || x > rowCnt || y < 1 || y > colCnt || a2[x][y] != 0){
+        return;
+    }
+
+    a2[x][y] = 2;
+    Fill2(x + 1, y);
+    Fill2(x - 1, y);
+    Fill2(x, y + 1);
+    Fill2(x, y - 1);
+}
+
+void Solve2(){
+    int i, j, hour = 0, flag;
+
+    Fill1(1, 1);
+    Fill2(1, 1);
+    ArrayPrn();
+    printf("aaaa\n");
+
+    while(1){
+        flag = 0;
+        for(i = 1; i <= rowCnt; i++){
+            for(j = 1; j <= colCnt; j++){
+                if(a1[i][j] == 1 && SideCount(i, j) >= 2){
+                    a2[i][j] = 0;
+                    flag = 1;
+                }
+            }
+        }
+
+        ArrayPrn();
+        if(flag == 0){
+            printf("%d", hour);
+            break;
+        }
+
+        for(i = 1; i <= rowCnt; i++){
+            for(j = 1; j <= colCnt; j++){
+                if(a1[i][j] == 1 && a2[i][j] == 0){
+                    Fill2(i, j);
+                }
+            }
+        }
+        hour++;
+        memcpy((void *)a1, (void *)a2, sizeof(a2));
+    }
+}
+
+int SideCount2(int x, int y){
+    int cnt = 0;
+    if(a2[x + 1][y] == 2)
+        cnt++;
+    if(a2[x - 1][y] == 2)
+        cnt++;
+    if(a2[x][y + 1] == 2)
+        cnt++;
+    if(a2[x][y - 1] == 2)
+        cnt++;
+
+    return cnt;
+}
+
+void Fill3(int x, int y){
+    if(x < 1 || x > rowCnt || y < 1 || y > colCnt)
+        return;
+
+    if(a2[x][y] == 3 || a2[x][y] == 0){
+        a2[x][y] = 2;
+
+        Fill3(x + 1, y);
+        Fill3(x - 1, y);
+        Fill3(x, y + 1);
+        Fill3(x, y - 1);
+    }
+}
+
+struct ROWCOL {
+    int row;
+    int col;
+};
+
+std::queue <ROWCOL> q;
+
+void QueuePrn(std::queue <ROWCOL> q1){
+    while(!q1.empty()){
+        printf("%d%d ", q1.front().row, q1.front().col);
+        q1.pop();
+    }
+    printf("\n");
+}
+
+void Solve3(){
+    int i, j, hour = 0, flag;
+    Fill3(1,1);
+    ArrayPrn();
+    printf("aaaa\n");
+    while(1){
+        flag = 0;
+        for(i = 1; i <= rowCnt; i++){
+            for(j = 1; j <= colCnt; j++){
+                if(a1[i][j] == 1 && SideCount2(i, j) >= 2){
+                    a2[i][j] = 3;
+                    q.push((ROWCOL){i,j});
+                    flag = 1;
+                }
+            }
+        }
+        ArrayPrn();
+        if(flag == 0){
+            printf("%d", hour);
+            break;
+        }
+
+        ROWCOL cur;
+        QueuePrn(q);
+        while(!q.empty()){
+            cur = q.front();
+            q.pop();
+            Fill3(cur.row, cur.col);
+        }
+        hour++;
+        memcpy((void *)a1, (void *)a2, sizeof(a2));
+    }
 }
 
 void Air(int x, int y){
@@ -109,13 +318,24 @@ void Ageing(){
         Melt();
         hours++;
    }
+}
 
-   printf("%d\n", hours);
+void Out(){
+    freopen("output.txt", "w", stdout);
+    printf("%d", hours);
+    fclose(stdout);
 }
 
 int main()
 {
-    Input();
+    /*
+    In();
     Ageing();
+    Out();
+    */
+
+    Input();
+    Solve1();
+
     return 0;
 }
