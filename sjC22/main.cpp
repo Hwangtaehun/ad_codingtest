71da8ff50d4191a47819beef0f285f4dc009d843
Hwangtaehun <r283@naver.com>
2024-01-23 19:34:13 +0900

    .....

--- sjC22/main.cpp
+++ sjC22/main.cpp
@@ -2,21 +2,69 @@
 #include <algorithm>
 
 bool chk[501];
-int data[501][2];
+int data_s[501][2];
 int line, rm_line = 0;
+
+//solve
+struct DATA{
+    int a;
+    int b;
+}data[101];
+int n;
+
+bool AscData(DATA left, DATA right){
+    return left.a < right.a;
+}
+
+void Input(){
+    freopen("input.txt", "r", stdin);
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++){
+        scanf("%d %d", &data[i].a, &data[i].b);
+    }
+}
+
+int Solve(int k){
+    int i, cnt = 1, cnt2;
+
+    for(i = k - 1; i >= 1; i--){
+        if(data[k].b > data[i].b){
+            cnt2 = Solve(i) + 1;
+            if(cnt < cnt2){
+                cnt = cnt2;
+            }
+        }
+    }
+
+    return cnt;
+}
+
+int Solve2(int k){
+    int i, cnt = 1, cnt2;
+    for(i = k + 1; i <= n; i++){
+        if(data[k].b < data[i].b){
+            cnt2 = Solve2(i) + 1;
+            if(cnt < cnt2){
+                cnt = cnt2;
+            }
+        }
+    }
+
+    return cnt;
+}
 
 void In(){
     freopen("input.txt", "r", stdin);
     scanf("%d", &line);
     for(int i = 1; i <= line; i++){
-        scanf("%d %d", &data[i][0], &data[i][1]);
+        scanf("%d %d", &data_s[i][0], &data_s[i][1]);
     }
     fclose(stdin);
 }
 
 void Print(){
     for(int i = 1; i <=line; i++){
-        printf("%d %d\n", data[i][0], data[i][1]);
+        printf("%d %d\n", data_s[i][0], data_s[i][1]);
     }
     printf("\n");
 }
@@ -24,13 +72,13 @@ void Print(){
 void Sort(){
     for(int i = 1; i <= line-1; i++){
         for(int j = i; j <= line; j++){
-            if(data[i][0] > data[j][0]){
-                int temp1 = data[i][0];
-                int temp2 = data[i][1];
-                data[i][0] = data[j][0];
-                data[i][1] = data[j][1];
-                data[j][0] = temp1;
-                data[j][1] = temp2;
+            if(data_s[i][0] > data_s[j][0]){
+                int temp1 = data_s[i][0];
+                int temp2 = data_s[i][1];
+                data_s[i][0] = data_s[j][0];
+                data_s[i][1] = data_s[j][1];
+                data_s[j][0] = temp1;
+                data_s[j][1] = temp2;
             }
         }
     }
@@ -40,7 +88,7 @@ void Eleicline(){
     Sort();
 
     for(int i = 1; i <= line; i++){
-        if(data[i][0] < data[i][1]){
+        if(data_s[i][0] < data_s[i][1]){
             chk[i] = true;
         }
     }
@@ -49,7 +97,7 @@ void Eleicline(){
         if(!chk[i]){
             for(int j = 1; j <= i; j++){
                 if(!chk[j]){
-                    if(data[i][1] < data[j][1]){
+                    if(data_s[i][1] < data_s[j][1]){
                         chk[i] = true;
                     }
                 }
@@ -64,7 +112,7 @@ void Eleicline(){
     }
 }
 
-void Out(){
+void Output(){
     freopen("output.txt", "w", stdout);
     printf("%d", rm_line);
     fclose(stdout);
@@ -72,8 +120,21 @@ void Out(){
 
 int main()
 {
-    In();
-    Eleicline();
-    Out();
+    //In();
+    //Eleicline();
+    //Output();
+    int i, cnt = 0, cnt2;
+    Input();
+    std::sort(data+1, data+n+1, AscData);
+
+    for(i =  1; i <= n; i++){
+        cnt2 = Solve(i);
+        if(cnt < cnt2){
+            cnt = cnt2;
+        }
+    }
+    //printf("%d\n", n - cnt);
+    rm_line = n - cnt;
+    Output();
     return 0;
 }
--- sjC23/main.cpp
+++ sjC23/main.cpp
@@ -1,16 +1,44 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
-int data[17][2];
+int data[1002][2];
 int event, sz, distance = 0x0fffffff;
+
+//solve
+int di[1002][1002], n, m, ans = 0x0fffffff;
+
+void Distance(){
+    for(int a = 0; a < m + 2; a++){
+        for(int b = 0; b < m + 2; b++){
+            di[a][b] = std::abs(data[a][0] - data[b][0]) + std::abs(data[a][1] - data[b][1]);
+        }
+    }
+}
 
 int dis(int a, int b){
     int d;
     d = abs(data[a][0] - data[b][0]) + abs(data[a][1] - data[b][1]);
 
     return d;
+}
+
+void Input(){
+    freopen("input.txt", "r", stdin);
+
+    scanf("%d%d", &n, &m);
+    data[0][0] = data[0][1] = 1;
+    data[1][0] = data[1][1] = n;
+    for(int i = 2; i < m +2; i++){
+        scanf("%d%d", &data[i][0], &data[i][1]);
+    }
+}
+
+void Solve(int a, int b, int d){
+    int next = (a > b? a : b) + 1;
+
 }
 
 void In(){
