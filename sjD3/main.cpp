3dfae9cf2203e1b24da4497dc6e64022ea85bc34
Hwangtaehun <r283@naver.com>
2024-01-25 19:39:01 +0900

    ....

--- sjD3/main.cpp
+++ sjD3/main.cpp
@@ -3,6 +3,13 @@
 char data[6];
 int sz, nsz, num_data[720];
 
+void Print(){
+    for(int i = 0; i < sz; i++){
+        printf("%c ", data[i]);
+    }
+    printf("\n");
+}
+
 bool Sosu(int num){
     for(int i = 2; i < num; i++){
         if(num % i == 0){
@@ -21,7 +28,7 @@ int Factorial(int num){
 
 void Input(){
     freopen("input.txt", "r", stdin);
-    scanf("%s", &data);
+    scanf("%s", data);
     fclose(stdin);
 
     for(int i = 0; i < 6; i++){
@@ -31,11 +38,54 @@ void Input(){
         }
     }
 
-    nsz = Factorial(sz);
+    for(int i = 0; i < sz - 1; i++){
+        for(int j = i; j < sz; j++){
+            if(data[j] < data[i]){
+                char temp = data[i];
+                data[i] = data[j];
+                data[j] = temp;
+            }
+        }
+    }
+}
+
+void Kind(){
+    int du1 = 1, du2 = 1, du3 = 1, j = 0, temp;
+
+    for(int i = j + 1; i < sz; i++){
+        if(data[j] == data[i]){
+            du1++;
+            temp = i;
+        }
+    }
+
+    j = temp + 1;
+
+    for(int i = j + 1; i < sz; i++){
+        if(data[j] == data[i]){
+            du2++;
+            temp = i;
+        }
+    }
+
+    j = temp + 1;
+
+    for(int i = j + 1; i < sz; i++){
+        if(data[j] == data[i]){
+            du3++;
+            temp = i;
+        }
+    }
+
+    nsz = Factorial(sz) / Factorial(du1) / Factorial(du2) / Factorial(du3);
+}
+
+void Combination(int n, int len){
 }
 
 int main()
 {
-    printf("sz = %d, nsz = %d", sz, nsz);
+    Input();
+    Kind();
     return 0;
 }
